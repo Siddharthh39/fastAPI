00613d76450700552588a9749e99929b13ec45cd
Siddharthh39 <megaberu81@gmail.com>
2025-09-26 17:58:02 +0530

    made some progress

---
+++ blog/schema.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+class blog(BaseModel):
+    title : str
+    body : str
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel, Field
+from datetime import date, time
+import uvicorn
 
 app = FastAPI()
 
 @app.get('/')
 def idx():
     return {'data':"blog List"}
+
+@app.get('/blog')
+def checkblogs(limit:int=10, published:bool=True, sort:Optional[str] = None):
+    if published:
+        return{'data': f"{limit} are published blogs"}
+    else:
+        return{'data': 'published'}
+
 
 @app.get('/blog/{id}')
 def about(id:int):
@@ -13,3 +25,16 @@ def about(id:int):
 @app.get('/blog/{id}/comments')
 def comment(id):
     return {'data':{'comments':{id}}}
+
+class BlogTake(BaseModel):
+    title : str
+    datePosted : date = Field(default_factory=date.today)
+    body : str
+
+@app.post('/blog')
+def postBlog(blog:BlogTake):
+    return{'data':f'title of this blog is {blog.title} date is {blog.datePosted}'}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)

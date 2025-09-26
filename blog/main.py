816d1201898d706944c4de66a8c011e244d72e71
Siddharthh39 <megaberu81@gmail.com>
2025-09-26 20:38:20 +0530

    fixed te changes

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from . import schema
-
+import uvicorn
 app = FastAPI()
 
 @app.post('/blog')
 def makePost(request:schema.blog):
-    return request
+    return {'name':{request.title},
+            'body':{request.body}}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='127.0.0.1', port=7000)
